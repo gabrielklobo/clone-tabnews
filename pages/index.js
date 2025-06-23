be1d7ac1892a734fb3108eb73edac35affbcb990
Gabriel Lobo <ga.p.lobo@gmail.com>
2025-06-23 20:44:16 -0300

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <h1>Sabe que eu te amo muit, e uso até a internet e meus estudos para lhe dizer isso meu amor, minha inspiração!! BEBA ÁGUA E COMA PIZZA!!!</h1>
+}
+
+export default Home;
