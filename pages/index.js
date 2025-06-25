3f0817363b4131d059b36dd63c54b7f433e7a468
Gabriel Lobo <ga.p.lobo@gmail.com>
2025-06-24 22:07:53 -0300

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Sabe que eu te amo muit, e uso até a internet e meus estudos para lhe dizer isso meu amor, minha inspiração!! BEBA ÁGUA E COMA PIZZA!!!</h1>
+    return <h1>❤️Silmara Manoel de Donno Sabe que eu te amo muito, e uso até a internet e meus estudos para lhe dizer isso meu amor, minha inspiração!! BEBA ÁGUA E COMA PIZZA!!!</h1>
 }
 
 export default Home;
