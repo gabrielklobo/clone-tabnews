a1480b95e9e8aadc828bcf05ec02145fbb28bca5
Gabriel Lobo <ga.p.lobo@gmail.com>
2026-02-19 19:14:04 -0300

    handles database ssl connection

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
   return (
     <h1>
-      Lembrando que estudo e esforço mudam vidas, as vezes precisamos recomeçar para entender seu verdadeiro significado.
+      Lembrando que estudo e esforço mudam vidas, as vezes precisamos recomeçar
+      para entender seu verdadeiro significado.
     </h1>
   );
 }
